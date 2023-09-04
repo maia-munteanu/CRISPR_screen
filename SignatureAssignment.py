b22f87ca0444799a74e387c8d6fda03559d61a43
Maia Munteanu <94048105+maia-munteanu@users.noreply.github.com>
2023-09-04 12:23:31 +0200

    Update SignatureAssignment.py

--- SignatureAssignment.py
+++ SignatureAssignment.py
@@ -31,4 +31,13 @@ def main_function():
 if __name__=="__main__":
    main_function()
 
+def main_function():
+     Analyze.cosmic_fit(samples_SNVs, output_cosmic, input_type="matrix", context_type="96",
+                   collapse_to_SBS96=True, cosmic_version=3.3, exome=False,
+                   genome_build=assembly, signature_database=None,
+                   exclude_signature_subgroups=None, export_probabilities=False,
+                   export_probabilities_per_mutation=True, make_plots=True,
+                   sample_reconstruction_plots=True, verbose=True)
+if __name__=="__main__":
+   main_function()
 
